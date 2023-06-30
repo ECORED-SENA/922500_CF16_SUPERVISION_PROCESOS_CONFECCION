6c55ffd5c13441bd71822b9fb5e987b223f939a4
Andrea Botello <andrea.botellor@misena.edu.co>
2023-06-30 08:17:53 -0500

    AD

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,7 +7,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/completar-espacios.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/completar-espacios.svg", alt="")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo.color-acento-contenido
           h2 Equipos en el laboratorio textil
@@ -17,7 +17,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Completar espacios
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
