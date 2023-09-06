2178a3df8e710e3dad3166342e2cf8c259b2381b
Andrea Botello <andrea.botellor@misena.edu.co>
2023-09-06 17:37:42 -0500

    AD

--- public/actividades/story_content/user.js
+++ public/actividades/story_content/user.js
@@ -2,6 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "63d3UUslT3c":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  window.close();
 }
 
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,7 +7,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/completar-espacios.svg", alt="")
+            img(src="@/assets/template/arrastrar-terminos.svg", alt="")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo.color-acento-contenido
           h2 Equipos en el laboratorio textil
@@ -15,7 +15,7 @@
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Completar espacios
+              p.fw-bold.mb-0 Relación de términos
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
